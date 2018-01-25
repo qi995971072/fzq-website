915442c495462e6a82732f32584caf57f41010e0
chenqi <chenqi@chenqi-mac.lan>
2018-01-25 18:07:48 +0800

    开始项目

---
+++ fzq-website/src/App.vue
@@ -0,0 +1,178 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <!--<div class="header-image">-->
+      <!--</div>-->
+      <img src="./assets/test-header.png">
+      <div style="padding-left: 15%; padding-right: 15%; box-sizing: border-box">
+        <div class="menu">
+          <div class="menu-item" v-for="item in menu_items">
+            <img src="./assets/test-menu-icon.png"/>
+            <div class="menu-title-container">
+              <div v-for="(value, index) in item">
+                <span v-if="index == 1" class="menu-title-big">{{value}}</span>
+                <span v-else class="menu-title">{{value}}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view/>
+    <div class="foot">
+      <div class="foot-subscribe">
+        <span style="border: solid 1px red">留下您的信息，订阅我们最新资讯</span>
+        <div style="border: solid 1px red">资讯订阅</div>
+      </div>
+      <div class="foot-contactus">
+        <div class="foot-contactus-item"></div>
+        <div class="foot-contactus-item"></div>
+        <div class="foot-contactus-item"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      menu_items: [
+        ['关于我们', 'Whe We Are', '使命*愿景', '义工*招募', '合作*伙伴', '意见*联系'],
+        ['服务对象', 'Service Object', '寺庙*师父', '学佛弟子', '居士群主', '善士信众', '使用帮助'],
+        ['核心功能', 'What We Do', '手机佛堂', '个人功课', '法本*课诵', '共修报数', '天天随喜'],
+        ['共修*活动', 'Practice', '每天手机供佛', '共修报数', '今天我吃素', '广种福田', '行走的力量', '手机祈福']
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app {
+  /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+
+  .header {
+    display: flex;
+    flex-direction: column;
+  }
+  .header-image {
+    width: 100%;
+    background-color: red;
+  }
+  .header img {
+    width: 100%;
+  }
+  .menu {
+    width: 100%;
+    /*height: 80px;*/
+    background-color: yellow;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  .menu-item {
+    width: 100%;
+    background-color: white;
+    /*border: solid 1px red;*/
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    flex-shrink: 1;
+    flex-grow: 1;
+    padding-top: 30px;
+  }
+  .menu-item img {
+    width: 37%;
+    /*height:w;*/
+  }
+  .menu-title-container {
+     margin-top: 25px;
+   }
+  .menu-title-big {
+    font-size: 21px;
+    text-align: center;
+    width: 100%;
+    display: block;
+    line-height: 45px;
+  }
+  .menu-title {
+    font-size: 15px;
+    text-align: center;
+    width: 100%;
+    display: block;
+    line-height: 26px;
+  }
+  .foot {
+    width: 100%;
+    background-color: green;
+  }
+  .foot-subscribe {
+    width: 100%;
+    height: 120px;
+    background-color: #C68045;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .foot-contactus {
+    width: 100%;
+    height: 320px;
+    background-color: #35161B;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+  .foot-contactus-item {
+    height: 100%;
+    width: 100%;
+    border: solid 1px red;
+  }
+
+/**
+* Eric Meyer's Reset CSS v2.0 (http://meyerweb.com/eric/tools/css/reset/)
+* http://cssreset.com
+*/
+
+/*
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video, input
+{
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font-weight: normal;
+  vertical-align: baseline;
+}*/
+
+  html, body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font-weight: normal;
+    vertical-align: baseline;
+  }
+</style>
---
+++ fzq-website/src/components/FirstPage.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="firstpage">
+    <!--<div class="aboutus-item">-->
+      <!--<span class="item-title">关于我们</span>-->
+      <!--<div class="aboutus">-->
+        <!--<div class="left-content">-->
+          <!--<div>-->
+            <!--<span class="left-content-title">使命*愿景</span>-->
+            <!--<span>佛子圈app, 是一群志同道合的学佛者, 一起开发的一款免费佛教修行工具APP. 目标是创建一个综合性的佛教移动服务平台, 为佛教事业贡献自己绵薄之力</span>-->
+          <!--</div>-->
+          <!--<hr/>-->
+          <!--<div>-->
+            <!--<span style="display: block">招募*义工</span>-->
+            <!--<ul>-->
+              <!--<li>各地弘法平台专员</li>-->
+              <!--<li>修行指导专员 - 各特定教派或传承</li>-->
+              <!--<li>新媒体运营经理</li>-->
+              <!--<li>活动策划专员</li>-->
+              <!--<li>客服专员</li>-->
+            <!--</ul>-->
+          <!--</div>-->
+          <!--<hr/>-->
+          <!--<div>-->
+            <!--<span style="display: block">合作*伙伴</span>-->
+            <!--<ol>-->
+              <!--<li>平台合作, 资源共享, 弘法利生, 共同进步</li>-->
+              <!--<li>共同发起丰富多彩的共修、公益慈善、募捐活动</li>-->
+            <!--</ol>-->
+          <!--</div>-->
+          <!--<hr/>-->
+          <!--<div>-->
+            <!--<span style="display: block">意见*联系</span>-->
+            <!--<span style="display: block">疑问：App的使用方面有任何疑问请联系：</span>-->
+            <!--<span style="display: block">cs@58fo.com</span>-->
+          <!--</div>-->
+        <!--</div>-->
+        <!--<div class="aboutus-right-img">-->
+          <!--&lt;!&ndash;<img src="../assets/test-header.png"/>&ndash;&gt;-->
+        <!--</div>-->
+      <!--</div>-->
+    <!--</div>-->
+
+    <div class="serviceObj">
+      <span class="item-title">服务对象</span>
+      <div style="display: flex; flex-direction: row">
+        <div class="serviceObj-leftimg">
+          <img src="../assets/test-header.png"/>
+        </div>
+        <div class="serviceObj-rightitems">
+          <span v-for="item in ['寺庙*师父', '学佛弟子', '居士群主', '善士信众', '常见问题']" class="serviceObj-rightitems-item">
+            {{item}}
+          </span>
+          <!--<div class="serviceObj-rightitems-item">寺庙*师父</div>-->
+          <!--<div class="serviceObj-rightitems-item">学佛弟子</div>-->
+          <!--<div class="serviceObj-rightitems-item">居士群主</div>-->
+          <!--<div class="serviceObj-rightitems-item">善士信众</div>-->
+          <!--<div >使用帮助</div>-->
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <span style="display: block; text-align: center">核心功能</span>
+      <div>
+        <img src="../assets/test-header.png"/>
+      </div>
+      <div>
+        <img src="../assets/test-career-nav.png"/>
+      </div>
+    </div>
+
+    <div>
+      <span style="display: block; text-align: center;">共修活动</span>
+      <div class="gxhd-itemcontaner">
+        <div class="gxhd-itemcontaner-row">
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>每天手机供佛</span>
+          </div>
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>共修报数</span>
+          </div>
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>今天我吃素</span>
+          </div>
+        </div>
+        <div class="gxhd-itemcontaner-row">
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>广种福田</span>
+          </div>
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>行走的力量</span>
+          </div>
+          <div class="gxhd-item">
+            <img src=""/>
+            <span>手机祈福</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .firstpage {
+    margin-top: 50px;
+    width: 100%;
+  }
+  .aboutus-item {
+    padding-left: 15%;
+    padding-right: 15%;
+    box-sizing: border-box;
+  }
+  .item-title {
+    display: block;
+    text-align: center;
+    font-size: 25px;
+    line-height: 60px;
+    /*background-color: red;*/
+  }
+  .aboutus {
+    display: flex;
+    flex-direction: row;
+    /*background-color: red;*/
+    justify-content: flex-start;
+    align-items: stretch;
+  }
+  .left-content {
+    /*width: 300px;*/
+    background-color: white;
+    font-size: 14px;
+
+    flex-shrink: 0;
+    flex-grow: 0;
+    flex-basis: 250px;
+
+    margin-right: 20px;
+  }
+  .left-content-title {
+    display: block;
+    line-height: 35px;
+  }
+  .aboutus-right-img {
+    background-color: peachpuff;
+    flex: 1;
+    /*flex-grow: 1;*/
+    /*flex-shrink: 1;*/
+    /*height: 100%;*/
+  }
+  /*.aboutus img {*/
+    /*width: 100%;*/
+    /*flex-grow: 1;*/
+    /*flex-shrink: 1;*/
+  /*}*/
+
+  .serviceObj {
+    padding-left: 15%;
+    padding-right: 15%;
+    box-sizing: border-box;
+    background-color: #FAF3E3;
+  }
+  .serviceObj-leftimg {
+    width: 80%;
+  }
+  .serviceObj-leftimg img {
+    width: 100%;
+    height: 100%;
+  }
+  .serviceObj-rightitems {
+    width: 20%;
+  }
+  .serviceObj-rightitems-item {
+    width: 100%;
+    height: 20%;
+    background-color: green;
+    text-align: center;
+    border: solid 1px red;
+    box-sizing: border-box;
+    display: block;
+  }
+  .gxhd-itemcontaner {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    padding-left: 10%;
+    padding-right: 10%;
+    box-sizing: border-box;
+  }
+  .gxhd-itemcontaner-row {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: space-around;
+  }
+  .gxhd-item {
+    width: 100%;
+    height: 200px;
+    background-color: green;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: solid 1px white;
+
+    margin-left: 10px;
+    margin-right: 10px;
+
+    flex-shrink: 1;
+    flex-grow: 1;
+  }
+  .gxhd-item img {
+    width: 100%;
+    height: 80%;
+    background-color: red;
+  }
+</style>
